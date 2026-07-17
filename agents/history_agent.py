5c1ac474cf39b9740bffaa50e1530d526c491289
24691a31s7 <midditejareddy9@gmail.com>
2026-07-17 08:22:47 +0530

    Delete agents directory

--- agents/history_agent.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-def get_history(symbol):
-
-    stock = yf.Ticker(symbol)
-
-    df = stock.history(period="5y")
-
-    return df
--- agents/technical_agent.py
+++
@@ -1,7 +0,0 @@
-from ta.momentum import RSIIndicator
-
-def calculate_rsi(df):
-
-    rsi = RSIIndicator(df["Close"])
-
-    return round(rsi.rsi().iloc[-1], 2)
